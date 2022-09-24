7bc8464c0c284e6082b73020aabdd51ea833b769
quyenUit <nguyenquyenboss@gmail.com>
2022-09-24 12:41:46 +0700

    update npm install server _ npm run build _ npx serve -s build

--- share-food-pwa-app/src/App.js
+++ share-food-pwa-app/src/App.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello react, this is pwa app demo</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
